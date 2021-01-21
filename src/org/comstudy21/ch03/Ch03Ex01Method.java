9e0b2cfe71704c6f5f43d227fb907612cbbbe3ce
BEOMJOON <comstudy21@naver.com>
2021-01-21 14:43:44 +0900

    Point 객체 swap

--- src/org/comstudy21/ch03/Ch03Ex01Method.java
+++ src/org/comstudy21/ch03/Ch03Ex01Method.java
@@ -1,10 +1,20 @@
 package org.comstudy21.ch03;
 
 public class Ch03Ex01Method {
+	
+	static void swap(int x, int y) {
+		int t = x;
+		x = y;
+		y = t;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("call by value example");
-		String user = "Hong-gildong";
-		System.out.println("User : " + user);
+		
+		int a=100;
+		int b=200;
+		
+		swap(a, b);
+		System.out.println("a:"+a+", b:"+b);
 	}
 }
